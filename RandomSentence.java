4fad8c7852bd8993ae21730dd3b1509284ed105d
ronisha-shigdel <ronishashigdel@gmail.com>
2020-11-11 13:00:10 +0545

    Variable and class name modified

---
+++ RandomSentence.java
@@ -0,0 +1,18 @@
+public class RandomSentence {
+	public static void main (String[] args){
+		String[] greetingList = {"Hi", "Bye", "Hello"};
+		String[] firstNameList = {"Ramesh", "Suresh", "Ganesh", "Mahesh"};
+		String[] lastNameList = {"Shrestha", "Sigdel", "Pokhrel"};
+		int greetingListLength = greetingList.length;
+		int firstNameListLength = firstNameList.length;
+		int lastNameListLength = lastNameList.length;
+		int randomGreeting = (int) (Math.random()*greetingListLength);
+		int randomFirstName = (int) (Math.random()*firstNameListLength);
+		int randomLastName = (int) (Math.random()*lastNameListLength);
+		System.out.println (greetingList[randomGreeting]+", "+firstNameList[randomFirstName]+" "+lastNameList[randomLastName]+".");
+	}
+}
+	
+		
+		
+	
